17fdfdd085123ac61775d5a844de3a23be896ef2
mateja <mateja176@gmail.com>
2019-05-06 17:53:37 +0200

    adds PrismEditor and RichTextEditor: with type errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import MyEditor from './MyEditor';
+import PrismEditor from './PrismEditor';
+import RichTextEditor from './RichTextEditor';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <main style={{ background: '#eee', height: '100vh' }}>
         <MyEditor />
+        <hr />
+        <PrismEditor />
+        <hr />
+        <RichTextEditor />
       </main>
     </div>
   );
